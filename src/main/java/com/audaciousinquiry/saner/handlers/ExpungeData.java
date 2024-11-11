abab4cd5481c4544fc52b1c0b488883316de4bdf
Austin Moody <amoody@ainq.com>
2024-11-11 09:58:38 -0500

    No longer getting SLF4J warnings in logs when running in AWS.

--- src/main/java/com/audaciousinquiry/saner/handlers/ExpungeData.java
+++ src/main/java/com/audaciousinquiry/saner/handlers/ExpungeData.java
@@ -1,9 +1,7 @@
 package com.audaciousinquiry.saner.handlers;
 
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.logging.LogLevel;
 import com.audaciousinquiry.saner.Utility;
 import com.audaciousinquiry.saner.config.Oauth2Config;
 import com.audaciousinquiry.saner.exceptions.SanerLambdaException;
@@ -11,6 +9,8 @@ import com.audaciousinquiry.saner.model.Job;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nimbusds.oauth2.sdk.ParseException;
 import com.nimbusds.oauth2.sdk.token.AccessToken;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import software.amazon.awssdk.regions.Region;
 
 import java.io.IOException;
@@ -23,13 +23,13 @@ import java.time.Duration;
 
 public class ExpungeData implements RequestHandler<Void, String> {
     private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final Logger log = LoggerFactory.getLogger(ExpungeData.class);
 
     @Override
     public String handleRequest(Void unused, Context context) {
-        LambdaLogger logger = context.getLogger();
         String returnValue;
 
-        logger.log("ExpungeData Lambda - Started");
+        log.info("ExpungeData Lambda - Started");
 
         try {
             String secretName = System.getenv("API_AUTH_SECRET");
@@ -37,10 +37,10 @@ public class ExpungeData implements RequestHandler<Void, String> {
             String expungeApiUrl = System.getenv("API_ENDPOINT");
 
             Oauth2Config oauth2Config = Oauth2Config.fromAwsSecret(region, secretName);
-            logger.log("Oauth2 Config Obtained From AWS Secret");
+            log.info("Oauth2 Config Obtained From AWS Secret");
 
             AccessToken accessToken = Utility.getOauth2AccessToken(oauth2Config);
-            logger.log("Access Token Obtained");
+            log.info("Access Token Obtained");
 
             HttpResponse<String> response;
             try (HttpClient httpClient = HttpClient.newBuilder()
@@ -60,33 +60,15 @@ public class ExpungeData implements RequestHandler<Void, String> {
             Job job = objectMapper.readValue(response.body(), Job.class);
             returnValue = job.getId();
 
-            logger.log(String.format(
-                    "API Call Status: %d, Saner Job ID: %s",
+            log.info("API Call Status: {}, Saner Job ID: {}",
                     response.statusCode(),
                     job.getId()
-                    )
             );
 
-            logger.log("ExpungeData Lambda - Completed");
-        } catch (URISyntaxException ex) {
-            logger.log(
-                    String.format("URI Syntax Exception: %s", ex.getMessage()), LogLevel.ERROR
-            );
+            log.info("ExpungeData Lambda - Completed");
+        } catch (URISyntaxException | IOException | ParseException ex) {
             throw new SanerLambdaException(ex.getMessage());
-        } catch (IOException ex) {
-            logger.log(
-                    String.format("IO Exception: %s", ex.getMessage()), LogLevel.ERROR
-            );
-            throw new SanerLambdaException(ex.getMessage());
-        } catch (ParseException ex) {
-            logger.log(
-                    String.format("ParseException: %s", ex.getMessage()), LogLevel.ERROR
-            );
-            throw new SanerLambdaException(ex.getMessage());
-        } catch (InterruptedException ex) {
-            logger.log(
-                    String.format("InterruptedException: %s", ex.getMessage()), LogLevel.ERROR
-            );
+        }   catch (InterruptedException ex) {
             Thread.currentThread().interrupt();
             throw new SanerLambdaException(ex.getMessage());
         }
